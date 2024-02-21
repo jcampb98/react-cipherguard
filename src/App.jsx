69c6ca2a5522c0374d8dc9b86c33da2c58edc4d0
jcampb98 <caboose768@gmail.com>
2024-02-21 11:57:12 +0000

    fixed a small issue with the styling of the password generator component

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,16 @@ import PasswordGenerator from './PasswordGenerator.jsx';
 import CollapsibleList from './CollapsibleList.jsx';
 
 function App() {
-  return (
-    <div className="app-container">
-      <div className="centered">
-        <PasswordGenerator />
-        <hr className='line-break'/>
-        <CollapsibleList />
-      </div>
-    </div>
-  );
+    return (
+        <div className="app-container">
+            <div className="centered">
+                <PasswordGenerator />
+                <div className="collapsible-container">
+                    <CollapsibleList />
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
